4d77019340cf124a1a2a798a55cc054fdef88e44
Eunwoo <dmsdn960@gmail.com>
2019-08-18 13:58:26 +0900

    syntax update

--- app.js
+++ app.js
@@ -22,52 +22,51 @@ function getCat(){
 getCat();
 
 navigator.geolocation.getCurrentPosition((position)=>{
-    const {latitude, longitude} = position.coords;
-    getWeather(latitude, longitude);
+    getWeather(position.coords);
 }, (e)=>console.log(e));
 
-function getWeather(lat, lng){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=2313f72f528ba9b181f713691469a1df&units=metric`)
-    .then(result => result.json())
-    .then(json => {
-        temp.innerHTML += `<div>${json.main.temp}℃</div>`;
-        place.innerHTML += `<div>${json.name}</div>`;
-        return json.weather.pop().main;
-    })
-    .then(main => {
-        let icon_class;
-        switch(main){
-            case 'Thunderstorm' : icon_class = 'mdi-weather-lightning'; break;
-            case 'Rain' :
-            case 'Drizzle' : icon_class = 'mdi-weather-pouring'; break;
-            case 'Snow': icon_class = 'mdi-weather-snowy'; break;
-            case 'Clear': icon_class = 'mdi-weather-sunny'; break;
-            case 'Clouds': icon_class = 'mdi-weather-cloudy'; break;
-            case 'Mist': 
-            default: icon_class = 'mdi-weather-fog'; break;
-        }
-        icon.classList.add(icon_class);
-    })
-    .catch(e => console.log(e));
+async function getWeather({latitude, longitude}){
+    const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=2313f72f528ba9b181f713691469a1df&units=metric`)
+
+    const json = await result.json();
+    
+    temp.innerHTML += `<div>${json.main.temp}℃</div>`;
+    place.innerHTML += `<div>${json.name}</div>`;
+
+    const main = json.weather.pop().main;
+    
+    let icon_class;
+    
+    switch(main){
+        case 'Thunderstorm' : icon_class = 'mdi-weather-lightning'; break;
+        case 'Rain' :
+        case 'Drizzle' : icon_class = 'mdi-weather-pouring'; break;
+        case 'Snow': icon_class = 'mdi-weather-snowy'; break;
+        case 'Clear': icon_class = 'mdi-weather-sunny'; break;
+        case 'Clouds': icon_class = 'mdi-weather-cloudy'; break;
+        case 'Mist': 
+        default: icon_class = 'mdi-weather-fog'; break;
+    }
+    icon.classList.add(icon_class);
 }
 
-input.onchange = function(e){
-    localStorage.setItem('name', e.target.value);
-    e.target.style.display = 'none';
+input.onchange = function({target}){
+    localStorage.setItem('name', target.value);
+    target.style.display = 'none';
     showName();
 }
 
-name.onclick = function(e){
-    e.target.style.display = 'none';
+name.onclick = function({target}){
+    target.style.display = 'none';
     input.style.display = 'inline-block';
-    input.value = e.target.innerHTML;
+    input.value = target.innerHTML;
 }
 
 function showName(){
-    const n = localStorage.getItem('name');
-    if(n){
+    const storage = localStorage.getItem('name');
+    if(storage){
         name.style.display = 'inline-block';
-        name.innerHTML = n;
+        name.innerHTML = storage;
         input.style.display = 'none';
     }
 }
