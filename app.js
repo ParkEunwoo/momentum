510becafe2f15607b34bd44cb7be571ab47b4584
513sojin <54045865+513sojin@users.noreply.github.com>
2019-08-21 11:02:04 +0900

    아이디를 자바스크립트로 불러옴
    뇸뇸

---
+++ app.js
@@ -0,0 +1,6 @@
+const icon = document.getElementById("icon");
+const temp = document.getElementById("temp");
+const place = document.getElementById("place");
+const time = document.getElementById("time");
+const name = document.getElementById("name");
+const input = document.getElementById("input_name");
