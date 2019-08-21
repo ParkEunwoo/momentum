81f2ca4d4a2a9fdd4e34c2a33fb20b091a8833c8
513sojin <54045865+513sojin@users.noreply.github.com>
2019-08-21 11:37:55 +0900

    time

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ const place = document.getElementById("place");
 const time = document.getElementById("time");
 const name = document.getElementById("name");
 const input = document.getElementById("input_name");
+
+function showTime(){
+    const now = new Date();
+    time.innerText = now.toLocaleTimeString().slice(3);
+    
+}
+
+
+setInterval(showTime,1000);
