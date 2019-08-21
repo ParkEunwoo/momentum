bae57e9cefe5ab9ebc86ed1b955cc6ba760f9071
513sojin <54045865+513sojin@users.noreply.github.com>
2019-08-21 12:52:54 +0900

    weather

--- app.js
+++ app.js
@@ -33,6 +33,37 @@ function showName(){
 
 }
 
-
-
 showName();
+
+
+navigator.geolocation.getCurrentPosition(position=>getWeather(position.coords),e=>console.log(e,message))
+
+async function getWeather({latitude,longitude}){
+    console.log(latitude,longitude)
+    const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=2313f72f528ba9b181f713691469a1df&units=metric`);
+    const json = await result.json();
+
+    temp.innerHTML = json.main.temp + '℃';
+    place.innerHTML = json.name;
+
+    data = result.json();
+    temp.innerText = data.main.temp + "ºC";
+    place.innerText = data.name;
+
+    switch(weather){
+        case 'Thunderstorm' : icon_class = 'mdi-weather-lightning'; break;
+        case 'Rain' :
+        case 'Drizzle' : icon_class = 'mdi-weather-pouring'; break;
+        case 'Snow': icon_class = 'mdi-weather-snowy'; break;
+        case 'Clear': icon_class = 'mdi-weather-sunny'; break;
+        case 'Clouds': icon_class = 'mdi-weather-cloudy'; break;
+        case 'Mist': 
+        default: icon_class = 'mdi-weather-fog'; break;
+    }
+
+    icon.classList.add(className);
+}
+
+
+
+
